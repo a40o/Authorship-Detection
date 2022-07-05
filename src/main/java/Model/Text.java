6ab7745d23441fec440f08ee4dd53ffa974617f3
a4ko <a4k0o@protonmail.com>
2022-07-05 16:23:52 +0300

    Add base Model

--- src/main/java/Model/Text.java
+++ src/main/java/Model/Text.java
@@ -13,26 +13,6 @@ public class Text {
     setValue(scanner.nextLine());
   }
 
-//  public String getAverageLengthOfWords() {
-//    int allWords = 0;
-//    double sumOfAllWords = 0;
-//    String[] words = getStrippedText1(text);
-//    for (String word : words) {
-//      double wordLength = word.length();
-//      sumOfAllWords += wordLength;
-//      allWords++;
-//    }
-//
-//    double averageWordLength = 0;
-//    if (allWords > 0) {
-//      averageWordLength = sumOfAllWords / allWords;
-//    }
-//
-//    DecimalFormat df = new DecimalFormat("#.###");
-//    return df.format(averageWordLength);
-//    return averageLengthOfWords;
-//  }
-
   public void setAverageLengthOfWords(String averageLengthOfWords) {
     this.averageLengthOfWords = averageLengthOfWords;
   }

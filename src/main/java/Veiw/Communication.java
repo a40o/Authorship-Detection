7cc510c948307a0c87cfdcf5eb8229cd4653771f
a4ko <a4k0o@protonmail.com>
2022-07-05 12:04:27 +0300

    Add communication

---
+++ src/main/java/Veiw/Communication.java
@@ -0,0 +1,8 @@
+package Veiw;
+
+public interface Communication {
+  void startupMessage();
+
+  void getAmountOfTexts();
+  void outputResults();
+}
--- src/main/java/Veiw/Communicator.java
+++ src/main/java/Veiw/Communicator.java
@@ -1,5 +1,26 @@
 package Veiw;
 
-public class Communicator {
+import java.util.Scanner;
 
+public class Communicator implements Communication{
+  Scanner scan = new Scanner(System.in);
+
+  @Override
+  public void startupMessage() {
+    System.out.println("Welcome");
+    getAmountOfTexts();
+  }
+
+  @Override
+  public void getAmountOfTexts() {
+    System.out.print("Please type number of texts: ");
+    int numberOfTexts = scan.nextInt();
+    //TODO return to model
+  }
+
+  @Override
+  public void outputResults() {
+    System.out.println("Here are the results: ");
+    //TODO add table for results
+  }
 }
